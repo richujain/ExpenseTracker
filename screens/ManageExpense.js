05cb3bc3fa75ee79999a9eb3f98f4cb69f33dd78
Richu <richujain1995@gmail.com>
2022-10-26 10:07:36 -0400

    Updating expenses added.

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -13,6 +13,10 @@ export default function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
+  const selectedExpense = expensesCtx.expenses.find(
+    (expense) => expense.id === editedExpenseId
+  );
+
   useLayoutEffect(() => {
     navigation.setOptions({
       title: isEditing ? "Edit Expense" : "Add Expense",
@@ -41,6 +45,7 @@ export default function ManageExpense({ route, navigation }) {
         submitButtonLabel={isEditing ? "Update" : "Add"}
         onCancel={cancelHandler}
         onSubmit={confirmHandler}
+        defaultValues={selectedExpense}
       />
 
       {isEditing && (
