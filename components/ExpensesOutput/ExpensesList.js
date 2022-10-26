ad83fcc6b5ee71939094a3f3b68ada38b0935458
Richu <richujain1995@gmail.com>
2022-10-26 05:08:50 -0400

    first commit

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,17 @@
+import { FlatList } from "react-native";
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+
+function renderExpenseItem(itemData) {
+  return <ExpenseItem {...itemData.item} />;
+}
+
+export default function ExpensesList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      keyExtractor={(item) => item.id}
+      renderItem={renderExpenseItem}
+    />
+  );
+}
